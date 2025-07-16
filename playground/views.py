ce5638d32053bdbae8ef8c56b79bce67cdab971c
mhriyad99 <mhriyad98@gmail.com>
2025-07-16 17:52:59 +0600

    django ORM

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+from store.models import Product, Customer, OrderItem
 
 # Create your views here.
 def say_hello(request):
-    x = 1
-    y = 2
-    return render(request, 'hello.html', {'name': 'Riyad'})
+    queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20))
+    return render(request, 'hello.html', {'name': 'Riyad', 'products': list(queryset)})
--- store/models.py
+++ store/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     title=models.CharField(max_length=255)
     slug=models.SlugField(default='-')
     description=models.TextField()
-    price=models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price=models.DecimalField(max_digits=6, decimal_places=2)
     inventory=models.IntegerField()
     last_update=models.DateTimeField(auto_now=True)
     collection=models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -60,7 +60,6 @@ class OrderItem(models.Model):
     order=models.ForeignKey(Order, on_delete=models.PROTECT)
     quantity=models.PositiveSmallIntegerField()
     unit_price=models.DecimalField(max_digits=6, decimal_places=2)
-
 
 class Address(models.Model):
     street=models.CharField(max_length=255)
