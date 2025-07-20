1874c59bef52fe426a5047b185ae8d9ca2cd38c7
mhriyad99 <mhriyad98@gmail.com>
2025-07-20 17:58:54 +0600

    django ORM

--- playground/views.py
+++ playground/views.py
@@ -4,15 +4,10 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, Prefetch, Value, F, Func
 from django.db.models.functions import Concat
 from django.db.models.aggregates import Count, Min, Max, Avg
-from store.models import Product, Customer, OrderItem, Order
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product, Customer, OrderItem, Order, Collection
+from tags.models import Tag, TaggedItem
 
 # Create your views here.
 def say_hello(request):
-    # queryset = Customer.objects.annotate(
-    #     full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
-    # )
-
-    queryset = Customer.objects.annotate(
-        orders_count=Count('order')
-    )
-    return render(request, 'hello.html', {'name': 'Riyad', 'result': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Riyad'})
--- store/models.py
+++ store/models.py
@@ -8,6 +8,9 @@ class Collection(models.Model):
     title=models.CharField(max_length=255)
     featured_product=models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self):
+        return self.title
+
 class Product(models.Model):
     title=models.CharField(max_length=255)
     slug=models.SlugField(default='-')
