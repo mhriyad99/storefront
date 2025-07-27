d7426f591cc11cfec521d3e9cfbad98aed5f5ef3
mhriyad99 <mhriyad98@gmail.com>
2025-07-27 16:57:30 +0600

    Django Serializer

---
+++ store/serializers.py
@@ -0,0 +1,23 @@
+from decimal import Decimal
+from rest_framework import serializers
+from store.models import Product, Collection
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'collection']
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(max_digits=5, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    collection = serializers.HyperlinkedRelatedField(
+        queryset=Collection.objects.all(),
+        view_name='collection-detail'
+    )
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
